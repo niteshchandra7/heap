f263d5bdec15312f5996ab9399c1f0df1084e487
niteshchandra7 <nitesh240295@gmail.com>
2023-12-17 16:13:39 +0530

    heap implementation

---
+++ heap.go
@@ -0,0 +1,81 @@
+package heap
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// Heap is a priority queue data structure.
+type Heap struct {
+	Container []any
+	Comp      func(child any, parent any) bool
+}
+
+// New return a pointer to an empty heap
+func New() *Heap {
+	return &Heap{}
+}
+
+// AddComparator sets the comparator function used for comparision during heapify operation
+func (h *Heap) AddComparator(Comp func(child any, parent any) bool) {
+	h.Comp = Comp
+}
+
+// Len returns length of heap
+func (h *Heap) Len() int {
+	return len(h.Container)
+}
+
+// Cap return capacity of Heap
+func (h *Heap) Cap() int {
+	return cap(h.Container)
+}
+
+// Push inserts the given item in heap
+func (h *Heap) Push(item any) error {
+	if h.Empty() == false && strings.EqualFold(fmt.Sprintf("%T", item), fmt.Sprintf("%T", h.Container[0])) == false {
+		return errors.New(fmt.Sprintf("expected type %s, got %s\n", fmt.Sprintf("%T", item), fmt.Sprintf("%T", h.Container[0])))
+	}
+	h.Container = append(h.Container, item)
+	child := h.Len() - 1
+	for child > 0 {
+		parent := (child - 1) / 2
+		if h.Comp(h.Container[child], h.Container[parent]) {
+			break
+		}
+		h.Container[parent], h.Container[child] = h.Container[child], h.Container[parent]
+		child = parent
+	}
+	return nil
+}
+
+// Empty checks whether the queue is empty
+func (h *Heap) Empty() bool {
+	return h.Len() == 0
+}
+
+// Return the item with highest priority
+func (h *Heap) Seek() (any, error) {
+	if h.Len() == 0 {
+		return 0, errors.New("empty container")
+	}
+	return h.Container[0], nil
+}
+
+// Pop removes the item with highest priority
+func (h *Heap) Pop() {
+	parent := 0
+	for parent < h.Len() {
+		child1 := 2*parent + 1
+		child2 := 2*parent + 2
+		if child2 != h.Len() && h.Comp(h.Container[child1], h.Container[child2]) {
+			h.Container[parent] = h.Container[child2]
+			parent = child2
+		} else {
+			h.Container[parent] = h.Container[child1]
+			parent = child1
+		}
+	}
+	h.Container = h.Container[0:h.Len()]
+}
